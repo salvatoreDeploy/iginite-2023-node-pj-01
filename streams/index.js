2480e25e8add264028b54b2ff323989382b09039
salvatoreDeploy <liderhenrique@gmail.com>
2023-02-06 00:54:23 -0300

    Stream no Node

---
+++ streams/index.js
@@ -0,0 +1,48 @@
+import { Readable, Transform, Writable } from "node:stream";
+
+// Stream de Leituda
+
+class OneTorHundredStream extends Readable {
+  index = 1;
+
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        const buf = Buffer.from(String(i));
+
+        this.push(buf);
+      }
+    }, 1000);
+  }
+}
+
+// new OneTorHundredStream().pipe(process.stdout);
+
+// Stream de Escrita
+
+class MultiplyTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
+
+// new OneTorHundredStream().pipe(new MultiplyTenStream());
+
+// Stream de Transformação
+
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
+new OneTorHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyTenStream());
