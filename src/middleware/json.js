f602ba67d92ebe214be257338482e0e33183388e
salvatoreDeploy <liderhenrique@gmail.com>
2023-02-11 21:46:38 -0300

    feat: implementation middleware

---
+++ src/middleware/json.js
@@ -0,0 +1,17 @@
+export async function json(req, res) {
+  // Leitura de dados
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  // Transforma os dados
+  try {
+    req.bodyRequest = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
+  res.setHeader("Content-type", "application/json");
+}
--- src/server.js
+++ src/server.js
@@ -1,28 +1,16 @@
 import http from "node:http";
+import { json } from "./middleware/json.js";
 
 const users = [];
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  // Leitura de dados
-  const buffers = [];
-
-  for await (const chunk of req) {
-    buffers.push(chunk);
-  }
-
-  // Transforma os dados
-  try {
-    req.bodyRequest = JSON.parse(Buffer.concat(buffers).toString());
-  } catch {
-    req.body = null;
-  }
+  await json(req, res);
 
   //console.log(bodyRequest);
 
   if (method === "GET" && url === "/") {
-    res.setHeader("Content-type", "application/json");
     return res.end(JSON.stringify(users));
   }
 
